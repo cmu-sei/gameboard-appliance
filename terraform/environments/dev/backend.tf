f4e7ee5f59280487671c5428abcf289c3a526f21
Mark Priest <mpriest@sei.cmu.edu>
2025-03-05 21:41:22 +0000

    change bucket name

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    key            = "infra/terraform.tfstate"
+    key            = "proxmox/terraform.tfstate"
     bucket         = "foundry-proxmox"
     dynamodb_table = "foundry-proxmox-locks"
   }
