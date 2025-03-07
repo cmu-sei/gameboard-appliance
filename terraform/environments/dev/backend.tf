544d4c9bdbb493c29dc652d4203a7f8a489e1e5f
Mark Priest <mpriest@sei.cmu.edu>
2025-03-07 00:57:29 +0000

    Ditch my non-working proxmox TF

--- terraform/environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    region         = "us-east-1"
-    key            = "dev/terraform.tfstate"
-    bucket         = "foundry-proxmox-terraform-state"
-    dynamodb_table = "foundry-proxmox-terraform-locks"
-  }
-}
--- terraform/environments/dev/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  # Use this role when deploying to the target AWS account
-  assume_role_arn = "arn:aws:iam::108782065069:role/central-admin-access"
-}
